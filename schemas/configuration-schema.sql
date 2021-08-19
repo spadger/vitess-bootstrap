80068a4b510644a001d5549b4c7442874e0df942
Jon Bates <jonathan.bates@team.bumble.com>
2021-08-19 16:14:30 +0100

    Support comments

--- schemas/configuration-schema.sql
+++ schemas/configuration-schema.sql
@@ -3,3 +3,6 @@ insert into post_id_seq(id, next_id, cache) values(0, 1, 10);
 
 create table channel_id_seq(id bigint, next_id bigint, cache bigint, primary key(id)) comment 'vitess_sequence';
 insert into channel_id_seq(id, next_id, cache) values(0, 1, 10);
+
+create table comment_id_seq(id bigint, next_id bigint, cache bigint, primary key(id)) comment 'vitess_sequence';
+insert into comment_id_seq(id, next_id, cache) values(0, 1, 10);
