cd87f2b219ab37592d326e4fa30db085f701679e
Jon Bates <jonathan.bates@team.bumble.com>
2021-09-01 14:53:38 +0100

    VSchema wasn't being applied

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ sleep 5
 alias vtctlclient="vtctlclient -server=localhost:15999 -logtostderr"
 alias mysql="mysql -h 127.0.0.1 -P 3306 -u user"
 
-vtctlclient ApplySchema -sql="$(cat schemas/configuration-schema.sql)" configuration
-vtctlclient ApplySchema -sql="$(cat schemas/usercontent-schema.sql)" usercontent
+vtctlclient -server=localhost:15999 -logtostderr ApplySchema -sql="$(cat schemas/configuration-schema.sql)" configuration
+vtctlclient -server=localhost:15999 -logtostderr ApplySchema -sql="$(cat schemas/usercontent-schema.sql)" usercontent
 
-vtctlclient ApplyVSchema -vschema="$(cat schemas/configuration-vschema.json)" configuration
-vtctlclient ApplyVSchema -vschema="$(cat schemas/initial-usercontent-vschema.json)" usercontent
+vtctlclient -server=localhost:15999 -logtostderr ApplyVSchema -vschema="$(cat schemas/configuration-vschema.json)" configuration
+vtctlclient -server=localhost:15999 -logtostderr ApplyVSchema -vschema="$(cat schemas/initial-usercontent-vschema.json)" usercontent
